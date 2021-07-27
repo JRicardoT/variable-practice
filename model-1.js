4a4e026bd1a9175a4c17904aa5b7d9404c76177b
jose Tovar <jose.tovar102397@gmail.com>
2021-07-26 21:08:44 -0600

    Add variables assigned to numbers

--- model-1.js
+++ model-1.js
@@ -1,3 +1,7 @@
 var exercise = "deadlift";
 var muscleTargeted = "legs, back";
-var difficultyLevel = " intermediate";
+var difficultyLevel = "intermediate";
+var olympicBar = "45";
+var numberOfPlates = "4";
+var weightOfPlates = "45";
+var numberOfReps = "8";
